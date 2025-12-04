0170ec04aa4be62cf9b912e74c07f45e5422cd87
ASHWIN R <125818122+ashwin12r@users.noreply.github.com>
2025-12-04 23:56:34 +0530

    Delete .next directory

--- .next/static/chunks/src_app_page_tsx_d846e3da._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_88045ea1._.js",
-  "static/chunks/node_modules_ac63e94b._.js"
-],
-    source: "dynamic"
-});
